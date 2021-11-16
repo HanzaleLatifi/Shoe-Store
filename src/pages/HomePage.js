3a9a95d4b2de64eb9b2faab64c0a869b352a8e43
h8 <h.latifi03@gmail.com>
2021-11-16 09:42:08 -0800

    onClick to btn add cart

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,9 @@
 import * as data from "../data";
 
 function HomePage() {
+  const addToCart = (p) => {
+    console.log(p);
+  };
   return (
     <main className="container">
       <section className="productList">
@@ -14,7 +17,12 @@ function HomePage() {
                 <p className="name">{product.name}</p>
                 <p className="price">{product.price} $</p>
               </div>
-              <button className="btn btn-primary">Add to cart</button>
+              <button
+                onClick={() => addToCart(product)}
+                className="btn btn-primary"
+              >
+                Add to cart
+              </button>
             </div>
           );
         })}
