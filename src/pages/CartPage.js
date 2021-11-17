9b53f71222f647c92e8e01a085c9726927b19f58
h8 <h.latifi03@gmail.com>
2021-11-17 09:40:17 -0800

    develope cartList

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,8 +1,37 @@
 import React from "react";
+import { useCart } from "../providers/CartProvider";
+import "./CartPage.css";
 
 function CartPage() {
+  const { cart } = useCart();
+
   return (
-      <main>cartpage</main>
+    <main className="cart-container">
+      <section className="cartList">
+        {cart.length ? (
+          cart.map((item) => {
+            return (
+              <div className="cartItem" key={item.id}>
+                <div className="cartImage">
+                  <img src={item.image} alt={item.name} />
+                </div>
+                <div>{item.name}</div>
+                <div>{item.price * item.quantity}</div>
+                <div className="btn-container">
+                  <button>Add</button>
+                  <button>{item.quantity}</button>
+                  <button>remove</button>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <p>cart is empty...!</p>
+        )}
+      </section>
+
+      <section className="CartDetail">more detail</section>
+    </main>
   );
 }
 
