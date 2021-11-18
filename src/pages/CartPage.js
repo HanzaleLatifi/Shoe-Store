d0f48cd688afa501398011ba81f2c2fb640230c0
h8 <h.latifi03@gmail.com>
2021-11-18 02:01:10 -0800

    DEC & INC to cart Bottons

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { useCart } from "../providers/CartProvider";
+import { useCart, useCartActions } from "../providers/CartProvider";
 import "./CartPage.css";
 
 function CartPage() {
   const { cart } = useCart();
+  const dispatch = useCartActions();
+
+  const incHandler = (item) => {
+    dispatch({ type: "ADD_TO_CART", payload: item });
+  };
+
+  const decHandler = (item) => {
+    dispatch({ type: "DEC_PRODUCT", payload: item });
+  };
 
   return (
     <main className="cart-container">
@@ -16,11 +25,11 @@ function CartPage() {
                   <img src={item.image} alt={item.name} />
                 </div>
                 <div>{item.name}</div>
-                <div>{item.price * item.quantity}</div>
+                <div>{item.price * item.quantity} $</div>
                 <div className="btn-container">
-                  <button>Add</button>
+                  <button onClick={() => incHandler(item)}>Add</button>
                   <button>{item.quantity}</button>
-                  <button>remove</button>
+                  <button onClick={() => decHandler(item)}>remove</button>
                 </div>
               </div>
             );
--- src/providers/CartReducer.js
+++ src/providers/CartReducer.js
@@ -1,6 +1,6 @@
 const cartReducer = (state, action) => {
   switch (action.type) {
-    case "ADD_TO_CART":
+    case "ADD_TO_CART": {
       const updatedCart = [...state.cart];
       const foundIndex = updatedCart.findIndex(
         (item) => item.id === action.payload.id
@@ -15,6 +15,25 @@ const cartReducer = (state, action) => {
       }
 
       return { ...state, cart: updatedCart };
+    }
+    case "DEC_PRODUCT": {
+      const updatedCart = [...state.cart];
+      const foundIndex = updatedCart.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      const updatedItem = { ...updatedCart[foundIndex] };
+
+      if (updatedItem.quantity === 1) {
+        const filteredCart = updatedCart.filter(
+          (item) => item.id !== action.payload.id
+        );
+        return { ...state, cart: filteredCart };
+      } else {
+        updatedItem.quantity--;
+        updatedCart[foundIndex] = updatedItem;
+        return { ...state, cart: updatedCart };
+      }
+    }
 
     default:
       return state;
