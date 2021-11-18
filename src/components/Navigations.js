65d059f05ba24f849bbb818ac826803f810650f1
h8 <h.latifi03@gmail.com>
2021-11-18 06:23:16 -0800

    show cartLength in NAV

--- src/components/Navigations.js
+++ src/components/Navigations.js
@@ -1,16 +1,24 @@
-import { NavLink, withRouter } from "react-router-dom"
+import { NavLink, withRouter } from "react-router-dom";
+import { useCart } from "../providers/CartProvider";
+import "./Navigations.css";
 
-import './Navigations.css'
 function Navigations() {
+  const { cart } = useCart();
+
   return (
-    <header >
+    <header>
       <nav>
         <ul>
           <li>
-            <NavLink to="/" activeClassName="NavActive" exact >Home</NavLink>
+            <NavLink to="/" activeClassName="NavActive" exact>
+              Home
+            </NavLink>
           </li>
-          <li>
-            <NavLink to="/cart" activeClassName="NavActive" >Cart</NavLink>
+          <li className="badge-container">
+            <NavLink to="/cart" activeClassName="NavActive">
+              Cart
+            </NavLink>
+            <span>{cart.length}</span>
           </li>
         </ul>
       </nav>
