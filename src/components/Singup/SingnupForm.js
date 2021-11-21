8ce9c10d80a4e805ecb59b233578c7d8d942d446
h8 <h.latifi03@gmail.com>
2021-11-21 20:29:09 +0330

    http user SignUp

--- src/components/Singup/SingnupForm.js
+++ src/components/Singup/SingnupForm.js
@@ -2,17 +2,18 @@ import { useFormik } from "formik";
 import { Link } from "react-router-dom";
 import * as Yup from "yup";
 import Input from "../../common/Input";
+import { signupUser } from "../../services/signupUser";
 import "./SiqnupForm.css";
+import { useState } from "react";
 
 const initialValues = {
   name: "",
   email: "",
   password: "",
   passwordConfirm: "",
+  phoneNumber: "",
 };
-const onSubmit = (values) => {
-  console.log(values);
-};
+
 const validationSchema = Yup.object({
   name: Yup.string().required("Name is Required"),
   email: Yup.string().required("Email is required").email("email is invalid"),
@@ -20,9 +21,30 @@ const validationSchema = Yup.object({
   passwordConfirm: Yup.string()
     .required("password Confirm is required")
     .oneOf([Yup.ref("password"), null], "Passwords must match"),
+  phoneNumber: Yup.string().required("is required"),
 });
 
 function SingnupForm() {
+  const [error, seterror] = useState(null);
+
+  const onSubmit = async (values) => {
+    const { name, email, phoneNumber, password } = values;
+    const data = {
+      name,
+      email,
+      password,
+      phoneNumber,
+    };
+    try {
+      await signupUser(data);
+      seterror(null);
+    } catch (error) {
+      if (error.response) {
+        seterror(error.response.data.message);
+      }
+    }
+  };
+
   const formik = useFormik({
     initialValues,
     onSubmit,
@@ -30,11 +52,14 @@ function SingnupForm() {
     validateOnMount: true,
     enableReinitialize: true,
   });
+
   return (
     <div style={{ minHeight: "60vh" }} className="Form-container">
       <form onSubmit={formik.handleSubmit} className="Form">
         <Input formik={formik} label="Name" name="name" />
         <Input formik={formik} label="Email" name="email" />
+        <Input formik={formik} label="phoneNumber" name="phoneNumber" />
+
         <Input
           formik={formik}
           label="Password"
@@ -55,6 +80,11 @@ function SingnupForm() {
         >
           Sign Up
         </button>
+        {error && (
+          <p className="error" style={{ fontSize: "2rem" }}>
+            {error}
+          </p>
+        )}
         <Link to="/login">Login</Link>
       </form>
     </div>
